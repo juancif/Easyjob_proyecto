da786b751b007f5350cca530ea53ca66d7ca2776
miguelangelbeltranrache11@gmail.com <cifuenteslozanojuandavid@gmail.com>
2024-09-06 14:50:25 -0500

    se integra codigo en los demas index

--- index_admin.php
+++ index_admin.php
@@ -16,21 +16,42 @@
                 <a href="http://localhost/GateGourmet/Crear_documento/crear.php" class="nav__link"><img src="imagenes/servicios.png" alt="Crear datetime" class="imgs__menu">Servicios </a>
             </li>
             <li class="nav__item">
-                <a href="http://localhost/GateGourmet/Gestor_usuarios/php/user/index_gestor.php" class="nav__link"><img src="imagenes/usuarios.png" alt="Gestor_usuarios" class="imgs__menu">Gestor usuarios</a>
+                <a href="http://localhost/GateGourmet/Gestor_usuarios/php/user/index_gestor.php" class="nav__link"><img src="imagenes/usuarios.png" alt="Gestor_usuarios" class="imgs__menu">Recomendados</a>
             </li>
             <li class="nav__item">
-                <a href="http://localhost/GateGourmet/log_eventos/log_eventos.php" class="nav__link"><img src="imagenes/eventos.png" alt="log_eventos" class="imgs__menu">Log de eventos</a>
+                <a href="http://localhost/GateGourmet/log_eventos/log_eventos.php" class="nav__link"><img src="imagenes/mensajes.png" alt="log_eventos" class="imgs__menu">Mensajes</a>
             </li>
             <li class="nav__item nav__buscar">
-                <input placeholder="Buscar" class="nav__link nav__link__buscar"><img src="imagenes/lupa.png" alt="log_eventos" class="imgs__buscar" ></input>
+                <input placeholder="Buscar servicio" class="nav__link nav__link__buscar"><img src="imagenes/lupa.png" alt="log_eventos" class="imgs__buscar" ></input>
             </li>
             <li class="nav__item__user">
                 <a href="http://localhost/GateGourmet/Movimientos/logout.php" class="cerrar__sesion__link"><img src="imagenes/user_verde.png" alt="Usuario" class="img__usuario"><div class="cerrar__sesion">Cerrar Sesión</div></a>
             </li>
         </ul>
     </nav>
-<div class="usuarios">
-
+    <div class="menu_lateral">
+        <div class="recuadro_menu"></div>
+        <div class="recuadro_menu"></div>
+        <div class="recuadro_menu"></div>
+        <div class="recuadro_menu"></div>
+    </div>
+<div class="noticias">
 </div>
+<div class="cuadro_perfil"></div>
+
+<div class="cuadro_perfil cuadro_perfil2"></div>
+<div class="cuadro_perfil cuadro_perfil3"></div>
+
+<div class="cuadro_perfil cuadro_perfil4"></div>
+
+<div class="cuadro_perfil cuadro_perfil5"></div>
+
+<div class="cuadro_perfil cuadro_perfil6"></div>
+
+<div class="cuadro_perfil cuadro_perfil7"></div>
+
+<div class="cuadro_perfil cuadro_perfil8"></div>
+
+<div class="cuadro_perfil cuadro_perfil9"></div>
 </body>
 </html>
